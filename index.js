f9ef00cc4a003e8754ac9b6eebcd6db5c97b56c9
Saul Saucedo <ssaucedo@outlook.com>
2025-04-22 12:39:40 -0400

    updates

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (process.env.SCOPE) {
 }
 
 // On successful install, users will be redirected to /oauth-callback
-const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
+const REDIRECT_URI = `https://cohortium-auth-c1533c3a90c7.herokuapp.com/oauth-callback`;
 
 //===========================================================================//
 
